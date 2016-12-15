abf53ecfb3b687b4e7c9a802b9158f90483dcf2d
Luciano Porto <luciano.bapo@gmail.com>
2016-12-15 03:27:26 -0200

    first commit

--- app/User.php
+++ app/User.php
@@ -26,4 +26,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function isAdmin()
+    {
+        return (array_search($this->email, explode(',', env('ADMIN_EMAILS', 'luciano.bapo@gmail.com')))!==false);
+    }
 }
--- config/erpnetWidgetResource.php
+++
@@ -1,5 +0,0 @@
-<?php
-return [
-    'siteName' => env('SITE_NAME', 'ERPNet'),
-    'siteLogo' => env('SITE_LOGO', 'https://storage.googleapis.com/ilhanet-140808.appspot.com/logo/logo-erpnet-compressed.png'),
-];
